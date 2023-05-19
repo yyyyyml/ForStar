3bb2404078658fd7ebf9e581449f0291a1d4d8ba
孟林 杨 <1762849856@qq.com>
2023-05-19 12:07:27 +0800

    update backend

---
+++ src/main/java/backend/instructions/SwInstruction.java
@@ -0,0 +1,22 @@
+package backend.instructions;
+
+import backend.RISCInstruction;
+import backend.RISCOperand;
+
+public class SwInstruction extends RISCInstruction{
+    public RISCOperand op1;
+    public RISCOperand op2;
+
+    public SwInstruction(RISCOperand op1, RISCOperand op2) {
+        this.operandNum = 2;
+        this.op1=op1;
+        this.setOperand(op1, 0);
+        this.op2=op2;
+        this.setOperand(op2, 1);
+        this.type = RISCInstruction.ITYPE.sw;
+    }
+
+    public String emit() {
+        return this.type.name()+" "+op1.emit()+","+op2.emit();
+    }
+}
