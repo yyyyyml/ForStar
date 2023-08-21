3e3b5b97029410b6e6cf0876ffb3acd0113bee82
孟林 杨 <1762849856@qq.com>
2023-08-21 12:54:29 +0800

    lsMerge

--- src/main/java/pass/backend/loadstore_merge/BackendLoadStoreMerge.java
+++ src/main/java/pass/backend/loadstore_merge/BackendLoadStoreMerge.java
@@ -13,47 +13,52 @@ public class BackendLoadStoreMerge implements BaseBackendPass {
         for (RISCFunction riscFunc : riscModule.getFunctionList()) {
             if (riscFunc.isBuildIn) continue;
             for (RISCBasicBlock riscBB : riscFunc.getBasicBlockList()) {
-                LinkedList<RISCInstruction> riscInstList = riscBB.getInstructionList();
-                RISCOperand tempStackAddress = null;
-                RISCOperand sdTempStackAddress = null;
-                int lastSdIndex = -1;
-                for (int instIndex = 0; instIndex < riscInstList.size(); instIndex++) {
-                    RISCInstruction riscInst = riscInstList.get(instIndex);
-                    if (riscInst instanceof LdInstruction && riscInst.getOperandAt(0).emit().equals("t2")) {
-                        if(tempStackAddress!=null && riscInst.getOperandAt(1).emit().equals(tempStackAddress.emit())){
-                            riscInstList.remove(instIndex);
-                            instIndex--;
-                            continue;
-                        }
-                        else {
-                            tempStackAddress = riscInst.getOperandAt(1);
-                        }
-                        if (sdTempStackAddress != null && riscInst.getOperandAt(1).emit().equals(sdTempStackAddress.emit())){
-                            sdTempStackAddress = null;
-                            lastSdIndex = -1;
-                        }
-                    }
-                    else if(riscInst instanceof SdInstruction ){
-                        if(!(riscInst.getOperandAt(0).emit().equals("t2"))
-                                && tempStackAddress!= null
-                                && riscInst.getOperandAt(1).emit().equals(tempStackAddress.emit())){
-                            tempStackAddress = null;
-                        }
-                        else if(riscInst.getOperandAt(0).emit().equals("t2")){
-                             if(sdTempStackAddress != null
-                                    && sdTempStackAddress.emit().equals(riscInst.getOperandAt(1).emit())){
-                                riscInstList.remove(lastSdIndex);
-                                instIndex--;
-                                lastSdIndex = instIndex;
-                                sdTempStackAddress = riscInst.getOperandAt(1);
-                            }
-                            else {
-                                lastSdIndex = instIndex;
-                                sdTempStackAddress = riscInst.getOperandAt(1);
-                            }
+                llssMerge(riscBB);
+                lsMerge(riscBB);
+            }
 
-                        }
+        }
+    }
+
+    private void lsMerge(RISCBasicBlock riscBB) {
+    }
+
+    private void llssMerge(RISCBasicBlock riscBB) {
+        LinkedList<RISCInstruction> riscInstList = riscBB.getInstructionList();
+        RISCOperand tempStackAddress = null;
+        RISCOperand sdTempStackAddress = null;
+        int lastSdIndex = -1;
+        for (int instIndex = 0; instIndex < riscInstList.size(); instIndex++) {
+            RISCInstruction riscInst = riscInstList.get(instIndex);
+            if (riscInst instanceof LdInstruction && riscInst.getOperandAt(0).emit().equals("t2")) {
+                if (tempStackAddress != null && riscInst.getOperandAt(1).emit().equals(tempStackAddress.emit())) {
+                    riscInstList.remove(instIndex);
+                    instIndex--;
+                    continue;
+                } else {
+                    tempStackAddress = riscInst.getOperandAt(1);
+                }
+                if (sdTempStackAddress != null && riscInst.getOperandAt(1).emit().equals(sdTempStackAddress.emit())) {
+                    sdTempStackAddress = null;
+                    lastSdIndex = -1;
+                }
+            } else if (riscInst instanceof SdInstruction) {
+                if (!(riscInst.getOperandAt(0).emit().equals("t2"))
+                        && tempStackAddress != null
+                        && riscInst.getOperandAt(1).emit().equals(tempStackAddress.emit())) {
+                    tempStackAddress = null;
+                } else if (riscInst.getOperandAt(0).emit().equals("t2")) {
+                    if (sdTempStackAddress != null
+                            && sdTempStackAddress.emit().equals(riscInst.getOperandAt(1).emit())) {
+                        riscInstList.remove(lastSdIndex);
+                        instIndex--;
+                        lastSdIndex = instIndex;
+                        sdTempStackAddress = riscInst.getOperandAt(1);
+                    } else {
+                        lastSdIndex = instIndex;
+                        sdTempStackAddress = riscInst.getOperandAt(1);
                     }
+
                 }
             }
         }
